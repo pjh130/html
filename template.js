8af5234a32d8a9828b5e89f9fda2705447113408
pjh130 <pjh130@163.com>
2021-04-06 20:25:25 +0800

    add

---
+++ template.js
@@ -0,0 +1,16 @@
+// JavaScript 入口函数:
+// JavaScript 的 window.onload 事件是等到所有内容，包括外部图片之类的文件加载完后，才会执行
+window.onload = function () {
+	// 执行代码
+}
+
+// jQuery 入口函数(必须包含jQuery的js)
+// jQuery 的入口函数是在 html 所有标签(DOM)都加载之后，就会去执行
+//完全写法
+$(document).ready(function(){
+	// 开始写 jQuery 代码...
+});
+//jQuery 简写
+$(function(){
+	// 开始写 jQuery 代码...
+});
